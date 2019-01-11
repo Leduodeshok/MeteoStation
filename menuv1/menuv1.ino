d966b65c51fe389d28e23c38b8721a4a5d5c2718
Leduodeshok <45876280+Leduodeshok@users.noreply.github.com>
2019-01-11 14:59:24 +0100

    Add files via upload

---
+++ menuv1/menuv1.ino
@@ -0,0 +1,28 @@
+
+LCDML_add         (0  , LCDML_0         , 1  , "Meteo Locale"      , mFunc_information);
+LCDML_add         (1  , LCDML_0_1       , 1  , "Temperature:"      , mFunc_Temploc);
+LCDML_add         (2  , LCDML_0_1       , 2  , "Pression:"         , mFunc_Pressloc);
+LCDML_add         (3  , LCDML_0_1       , 3  , "Vent:"             , mFunc_Ventloc);
+LCDML_add         (4  , LCDML_0_1       , 4  , "Diraction:"        , mFunc_Dirloc);
+LCDML_add         (5  , LCDML_0_1       , 5  , "Retour"            , mFunc_back);
+LCDML_add         (6  , LCDML_0         , 2  , "Meteo en Ligne"    , mFunc_Pressloc);   
+LCDML_add         (7  , LCDML_0_2       , 1  , "Ville"             , mFunc_Ville);
+LCDML_add         (8  , LCDML_0_2       , 2  , "Temperature:"      , mFunc_Templi);
+LCDML_add         (9  , LCDML_0_2       , 3  , "Humidite:"         , mFunc_Humili);
+LCDML_add         (10 , LCDML_0_2       , 4  , "Pression:"         , mFunc_Pressli);
+LCDML_add         (11 , LCDML_0_2       , 5  , "Vent:"             , mFunc_Ventli);
+LCDML_add         (12 , LCDML_0_2       , 7  , "Direction:"        , mFunc_Dirli);
+LCDML_add         (13 , LCDML_0_2       , 8  , "Temps:"            , mFunc_Weali);
+LCDML_add         (14 , LCDML_0_2       , 9  , "CHANGER"           , mFunc_Ville);
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
